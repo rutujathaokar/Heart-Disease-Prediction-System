0341615096cddf5665d2205d3045e17e722ebe32
Rutuja Thaokar <thaokarrutuja18@gmail.com>
2026-06-08 16:13:38 +0530

    Update app.py

--- CVD prediciton/CVD/app.py
+++ CVD prediciton/CVD/app.py
@@ -115,8 +115,7 @@ def predict():
 
     except Exception as e:
         return render_template('index.html', error=f"Error making prediction: {str(e)}")
-
+        
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run()
     
